5d887ef59f2a17de603a2f9e833bba0a068d3c2c
AndreasJepsenNielsen <andr921c@stud.kea.dk>
2019-05-24 16:02:23 +0200

    ll

--- src/app/update-quiz/update-quiz.component.ts
+++ src/app/update-quiz/update-quiz.component.ts
@@ -6,7 +6,7 @@ import { Quiz, Option, Question } from '../entities/quiz';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { QuizActions } from '../quiz.actions';
 import { QuizApiService } from '../quiz-api.service';
-import { Gender } from '../entities/user';
+
 
 @Component({
   selector: 'app-update-quiz',
@@ -20,7 +20,8 @@ export class UpdateQuizComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private ngRedux: NgRedux<AppState>, private fb: FormBuilder,private quizActions: QuizActions, private quizApi: QuizApiService) { }
 
   get questions() { return (this.updateQuiz.get('questions')) as FormArray; }
-  get options() { return (this.updateQuiz.get(['questions', 'options'])) as FormArray; }
+  get options () { return (<FormArray>(this.questions).controls['options'])}
+  
 
   
 
@@ -34,28 +35,33 @@ export class UpdateQuizComponent implements OnInit {
 
     console.log(this.quiz._id)
     console.log(this.quiz.questions)
-
+    
     this.updateQuiz = this.fb.group({
-      title: [this.quiz.title, Validators.required],
+      title: this.quiz.title,
       questions: this.fb.array([
-        this.fb.group({options: this.fb.array([
+        this.fb.group({
+          title: ""
+          ,options: this.fb.array([
           this.fb.group({
             answer: "",
             correct: false
           })
-        ])})
+        ])
+      })
       ]),  
     })
 
     for( let i = 0; i < this.quiz.questions.length; i++){
 
       this.addProduct(this.quiz.questions[i])
-
         for (let j = 0; j < this.quiz.questions[i].options.length; j++) {
+          console.log(this.quiz.questions[i].options[j])
           this.addOption(this.quiz.questions[i].options[j]);
         }
     }
+    this.questions.removeAt(0)
     console.log(this.updateQuiz)
+
   }
 
 
@@ -67,10 +73,17 @@ export class UpdateQuizComponent implements OnInit {
   }
 
   addOption(j: Option) {
+    if(this.options){
+      console.log(j)
+      console.log(j.answer)
+      let option: Option;
+      option.answer = j.answer
+      option.correct = j.correct
     this.options.push(this.fb.group({
-      answer: j
+      answer: j.answer, correct: j.correct
     }))
   }
+}
 
   updateQuizSubmit() {
     let quizUpdate = this.updateQuiz.value as Quiz;
