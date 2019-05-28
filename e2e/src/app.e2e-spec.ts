6186692034dafa7de3b56a75af763603d173ce00
Lukas <luka0786@stud.kea.dk>
2019-05-28 12:32:38 +0200

    wowtoxic

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -13,21 +13,113 @@ describe('workspace-project App', () => {
 
   it('1.0: Verify that I can go to the login component', () => {
     browser.get('home/login');
-    let loginText = element(by.id('login')).getText();
+    let loginText = element(by.id('username')).getText();
 
-    expect(loginText).toEqual("Login");
+    expect(loginText).toEqual("");
   });
 
-  it('login by filling out username and password, landing page afterwards', () => {
-    element(by.id('username')).sendKeys('admin');
-    element(by.id('password')).sendKeys('admin');
-    
+  it('1.1: login by filling out username and password, landing page afterwards', () => {
+    element(by.id('username')).sendKeys('administrator');
+    element(by.id('password')).sendKeys('administrator');
+
     element(by.id('submitLogin')).click();
-    
+
     // Cleaner way to check that you are in the right place.
     expect(browser.getCurrentUrl()).toContain('/portal/display-quizzes');
 
   });
+
+  it('1.2: access a quiz, navigate back and access the second quiz', () => {
+    element(by.id('1')).click();
+
+    browser.navigate().back();
+
+    element(by.id('2')).click();
+  });
+
+
+  it('1.3: create a new quiz and access it', () => {
+    element(by.id('createQuiz')).click();
+
+    expect(browser.getCurrentUrl()).toContain('/portal/create-quiz');
+
+    element(by.id('quizTitle')).sendKeys('e2eTitle');
+    element(by.id('question1')).sendKeys('e2eQuestion');
+    element(by.id('option1_1')).sendKeys('e2eOption');
+    element(by.id('option1_2')).sendKeys('e2eOption');
+    element(by.id('option1_3')).sendKeys('e2eOption');
+    element(by.id('option1_4')).sendKeys('e2eOption');
+    element(by.id('question2')).sendKeys('e2eQuestion2');
+    element(by.id('option2_1')).sendKeys('e2eOption');
+    element(by.id('option2_2')).sendKeys('e2eOption');
+    element(by.id('option2_3')).sendKeys('e2eOption');
+    element(by.id('option2_4')).sendKeys('e2eOption');
+
+    element(by.id('saveQuiz')).click();
+
+    expect(browser.getCurrentUrl()).toContain('/portal/display-quizzes');
+
+
+    element(by.id('3')).click();
+
+
+  })
+it('1.4: logout', () => {
+    element(by.id('options')).click();
+
+    element(by.id('logout')).click();
+
+    expect(browser.getCurrentUrl()).toContain('/login');
+  });
+
+  it('2.0: Verify that I can go to the login component', () => {
+    browser.get('/login');
+    let loginText = element(by.id('username')).getText();
+
+    expect(loginText).toEqual("");
+  });
+
+  it('2.1: Try to login using bad credentials', () => {
+    element(by.id('username')).sendKeys('admin');
+    element(by.id('password')).sendKeys('admin');
+
+    element(by.id('submitLogin')).click();
+
+    // Cleaner way to check that you are in the right place.
+    expect(browser.getCurrentUrl()).toContain('/login');
+  })
+
+
+  it('3.0: Verify that I can go to the login component', () => {
+    browser.get('/login');
+    let loginText = element(by.id('username')).getText();
+
+    expect(loginText).toEqual("");
+  });
+
+  it('3.1: login by filling out username and password, as a normal user', () => {
+    element(by.id('username')).sendKeys('username');
+    element(by.id('password')).sendKeys('password');
+
+    element(by.id('submitLogin')).click();
+
+    expect(browser.getCurrentUrl()).toContain('/portal/display-quizzes');
+  });
+
+  it('3.2: Try to navigate to admin page', () => {
+    browser.navigate().to('http://localhost:4200/portal/admin');
+
+    expect(browser.getCurrentUrl()).toContain('/login');
+  });
+
+  it('3.3: logout', () => {
+    element(by.id('options')).click();
+
+    element(by.id('logout')).click();
+
+    expect(browser.getCurrentUrl()).toContain('/login');
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
